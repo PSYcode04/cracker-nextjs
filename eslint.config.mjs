2bd2d9e061aa16a308b77c84f87b9cba1136d726
seyeon <psy.code04@gmail.com>
2024-10-11 17:56:52 +0900

    chore : prettier 설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,16 @@
-import globals from "globals";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import reactPlugin from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-import react from "eslint-plugin-react";
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import reactPlugin from 'eslint-plugin-react/configs/recommended.js'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import { fixupConfigRules } from '@eslint/compat'
+import react from 'eslint-plugin-react'
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(reactPlugin),
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { ...react } },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { ...react } },
   {
     languageOptions: {
       parserOptions: { ecmaFeatures: { jsx: true } },
@@ -19,13 +20,15 @@ export default [
   {
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
   {
     rules: {
-      "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
     },
   },
-];
+  { ignores: ['node_modules', 'dist', 'public'] },
+  eslintPluginPrettier,
+]
