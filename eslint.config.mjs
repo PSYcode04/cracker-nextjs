c5717b1fe2727acc0c64db1bde453d30af70c332
seyeon <psy.code04@gmail.com>
2024-10-11 17:12:54 +0900

    chore : eslint 설정중...

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import react from 'eslint-plugin-react';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(reactPlugin),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { react } },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } }, globals: { ...globals.browser } } },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
