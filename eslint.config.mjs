f26da0233b8ad7e8687b018da8c174473af5bb4d
seyeon <psy.code04@gmail.com>
2024-10-14 13:17:39 +0900

    chore : eslint.config.mjs 구조 변경

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,30 +7,23 @@ import { fixupConfigRules } from '@eslint/compat'
 import react from 'eslint-plugin-react'
 
 export default [
-  eslint.configs.recommended,
+  eslint.configs.recommended, // eslint 팀에서 권장하는 규칙세트
   ...tseslint.configs.recommended,
   ...fixupConfigRules(reactPlugin),
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], // ESLint가 검사할 파일 확장자 설정
     plugins: { ...react, ...eslintPluginPrettier },
-  },
-  {
     languageOptions: {
+      ecmaVersion: 'latest', // default
       parserOptions: { ecmaFeatures: { jsx: true } },
       globals: { ...globals.browser },
     },
-  },
-  {
     settings: {
-      react: {
-        version: 'detect',
-      },
+      react: { version: 'detect' }, // 사용자가 설치한 버전으로 선택
     },
-  },
-  {
     rules: {
       'react/react-in-jsx-scope': 'off',
     },
+    ignores: ['node_modules', 'dist', 'public', '.next'],
   },
-  { ignores: ['node_modules', 'dist', 'public', '.next'] },
 ]
